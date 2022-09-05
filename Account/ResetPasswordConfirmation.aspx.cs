30fccb25ddbee532387fcd80e7f9801bbe6cead3
juanp <juanp@DESKTOP-JATAE0K>
2022-09-04 23:02:50 -0600

    Add project files.

---
+++ Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ProyectoPeluqueriaFinal.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoPeluqueriaFinal.Startup))]
+namespace ProyectoPeluqueriaFinal
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
